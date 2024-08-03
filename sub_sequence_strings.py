0299315e6c0104b9c6c3d6716079bbef51189ed9
parth-kachhadiya <147975077+parth-kachhadiya@users.noreply.github.com>
2024-08-03 20:19:15 +0530

    Add files via upload
    Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
    
    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).

---
+++ sub_sequence_strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str):
+        i,j = 0,0
+        if len(s) == 0:
+            return True
+        else:
+            while i < len(t) and j < len(s):
+                if s[j] == t[i]:
+                    j += 1
+                i += 1
+
+            return j == len(s)

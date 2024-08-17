20c0b4ea773708e9abbabeb0fdcd7333ac8a033c
parth-kachhadiya <147975077+parth-kachhadiya@users.noreply.github.com>
2024-08-17 13:33:28 +0530

    Update nth_tribonacci.py

--- nth_tribonacci.py
+++ nth_tribonacci.py
@@ -1,27 +1,13 @@
-def tribonacci(n):
-    if n == 0:
-        print(0)
-    elif n == 1:
-        print(0, 1)
-    elif n == 2:
-        print(0, 1, 1)
-    else:
-        a, b, c, d = 0, 1, 1, 0
-        print(f'{a}, {b}, {c}',end=", ")
-        for _ in range(3,n+1):
-            d = a + b + c
-            print(d,end=", ")
-            a, b, c = b, c, d
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n <= 2:
+            return 0 if n == 0 else 1
+        else:
+            a, b, c, d = 0, 1, 1, 0
+            for _ in range(2,n):
+                d = a + b + c
+                if _ == (n-1):
+                    return d
+                a, b, c = b, c, d
 
-def tribonacci_2(n):
-    if n <= 2:
-        return 0 if n == 0 else 1
-    else:
-        a, b, c, d = 0, 1, 1, 0
-        for _ in range(2,n):
-            d = a + b + c
-            if _ == (n-1):
-                return d
-            a, b, c = b, c, d
-
-print(tribonacci_2(25)) 
+print(Solution().tribonacci(25)) 
